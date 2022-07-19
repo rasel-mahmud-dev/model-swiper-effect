eb090c64042ec15feb17356628fa6ea034da5268
rasel mahmud dev <rasel.mahmud.dev@gmail.com>
2022-07-19 19:57:06 +0600

    added live server

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
- 
-
-
-
- 
-
-
-
-
-
 #!/usr/bin/env sh
 
 # abort on errors
@@ -52,6 +21,8 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:rasel-mahmud-dev/model-swiper-effect.git main:gh-pages
+
+rm -rf dist
 
 cd -
